cad9878dd589a8abb3ea541f022093ec5e77fd3c
echo8358 <codykhiggins@gmail.com>
2024-01-19 22:17:48 -0700

    King and bishop move tests pass

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -10,8 +10,9 @@ import java.lang.reflect.Array;
  */
 public class ChessBoard {
 
+    private ChessPiece[][] board;
     public ChessBoard() {
-
+        board = new ChessPiece[8][8];
     }
 
     /**
@@ -21,7 +22,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        board[position.getRow()-1][position.getColumn()-1] = piece;
     }
 
     /**
@@ -32,7 +33,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return board[position.getRow()-1][position.getColumn()-1];
     }
 
     /**
--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,10 @@
 package chess;
 
+import java.sql.Array;
 import java.util.Collection;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -12,6 +16,7 @@ public class ChessPiece {
 
     private final ChessGame.TeamColor teamColor;
     private final ChessPiece.PieceType pieceType;
+
 
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
         this.teamColor = pieceColor;
@@ -50,6 +55,60 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        HashSet<ChessMove> moves = new HashSet<>();
+        switch (getPieceType()){
+            case KING:
+                for(int i = -1; i < 2; i++) {
+                    for(int j = -1; j < 2; j++) {
+                        checkAndAddMove(board, moves, myPosition, i,j);
+                    }
+                }
+                break;
+
+            case BISHOP:
+                for(int i = 1; i < 8; i++) {
+                    if (checkAndAddMove(board, moves, myPosition, i, i) == 1) break;
+                }
+                for(int i = 1; i < 8; i++) {
+                    if (checkAndAddMove(board, moves, myPosition, -i, i) == 1) break;
+                }
+                for(int i = 1; i < 8; i++) {
+                    if (checkAndAddMove(board, moves, myPosition, i, -i) == 1) break;
+                }
+                for(int i = 1; i < 8; i++) {
+                    if (checkAndAddMove(board, moves, myPosition, -i, -i) == 1) break;
+                }
+                break;
+        }
+        return moves;
+    }
+
+    private int checkAndAddMove(ChessBoard board, HashSet<ChessMove> moves, ChessPosition myPosition, int x, int y)
+    {
+        if(myPosition.getRow()+x >=1 && myPosition.getRow()+x <= 8 && myPosition.getColumn()+y >= 1 && myPosition.getColumn()+y <= 8)
+        {
+            ChessPiece targetPiece = board.getPiece(new ChessPosition(myPosition.getRow()+x, myPosition.getColumn()+y));
+            if(targetPiece == null || targetPiece.getTeamColor() != teamColor)
+            {
+                moves.add(new ChessMove(myPosition, new ChessPosition(myPosition.getRow()+x, myPosition.getColumn()+y), null));
+                if (targetPiece != null) {return 1;}
+                else {return 0;}
+            }
+            return 1;
+        }
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessPiece that = (ChessPiece) o;
+        return teamColor == that.teamColor && pieceType == that.pieceType;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(teamColor, pieceType);
     }
 }
