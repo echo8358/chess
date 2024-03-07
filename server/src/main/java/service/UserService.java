0d0d331dc8c18c204725435bd01caf4610e91b7f
echo8358 <codykhiggins@gmail.com>
2024-03-07 09:56:44 -0700

    Hashing passwords

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -6,6 +6,7 @@ import dataAccess.Exceptions.DataAccessException;
 import dataAccess.Exceptions.UnauthorizedException;
 import model.AuthData;
 import model.UserData;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import server.Login.LoginRequest;
 import server.Login.LoginResponse;
 import server.Logout.LogoutRequest;
@@ -18,10 +19,12 @@ import java.util.Objects;
 public class UserService {
     static UserDAO userDAO = new SQLUserDAO();
     static AuthDAO authDAO = new SQLAuthDAO();
+    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
     public RegisterResponse register(RegisterRequest registerRequest) throws AlreadyTakenException, DataAccessException {
         AuthData newAuthData;
-        UserData user = new UserData(registerRequest.username(), registerRequest.password(), registerRequest.email());
+        String hashedPassword = encoder.encode(registerRequest.password());
+        UserData user = new UserData(registerRequest.username(), hashedPassword, registerRequest.email());
         userDAO.createUser(user);
         newAuthData = authDAO.createAuth(user.username());
         return new RegisterResponse(newAuthData);
@@ -29,7 +32,7 @@ public class UserService {
 
     public LoginResponse login(LoginRequest loginRequest) throws UnauthorizedException, DataAccessException {
         UserData reqUser = userDAO.getUser(loginRequest.username());
-        if (reqUser == null || !Objects.equals(reqUser.password(), loginRequest.password())) {
+        if (reqUser == null || !encoder.matches(loginRequest.password(),reqUser.password())) {
             throw new UnauthorizedException("Error: unauthorized");
         }
         return new LoginResponse(authDAO.createAuth(loginRequest.username()));
