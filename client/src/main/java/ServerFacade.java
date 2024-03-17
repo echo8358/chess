ce93158b84770bb2609d539ba791d5b7664ba36d
echo8358 <codykhiggins@gmail.com>
2024-03-16 23:06:10 -0600

    created skeleton server facade.

---
+++ client/src/main/java/ServerFacade.java
@@ -0,0 +1,28 @@
+import model.AuthData;
+import model.GameData;
+
+import java.util.ArrayList;
+
+public class ServerFacade {
+
+    public AuthData register(String username, String password, String email) {
+        return null;
+    }
+    public void logout(String authToken) {
+    }
+
+    public AuthData login(String username, String password) {
+        return null;
+    }
+
+    public ArrayList<GameData> listGames(String authToken) {
+        return null;
+    }
+
+    public void joinGame(String playerColor, int gameID, String auth) {
+    }
+
+    public int createGame(String gameName, String authToken) {
+        return 0;
+    }
+}
